c8ec878480321c95ea06f72ff70be1ab4e7ec01c
Gabor Racz <gabzracz.dev@gmail.com>
2023-05-13 00:20:28 +0200

    update kts 2

--- .space.kts
+++ .space.kts
@@ -3,6 +3,18 @@ import jetbrains.space.vcs.checkout.GitCheckout
 import jetbrains.space.vcs.checkout.GitHubCheckout
 
 spaceApplication {
+    container("openjdk:11") {
+    kotlinScript { api ->
+        api.space().projects.automation.deployments.start(
+            project = api.projectIdentifier(),
+            targetIdentifier = TargetIdentifier.Key("do-frontend-deployment"),
+            version = "1.0.0",
+            // automatically update deployment status based on a status of a job
+            syncWithAutomationJob = true
+        )
+    }
+}
+
     
     // Define the deployment configuration for the Angular app
     deployment("angular-deployment") {
