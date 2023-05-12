7a3119a74ad22af146fc000523a79d3620577cf1
Gabor Racz <gabzracz.dev@gmail.com>
2023-05-13 00:02:51 +0200

    space kts file - deployment update

--- .space.kts
+++ .space.kts
@@ -1,11 +1,78 @@
-container("openjdk:11") {
-    kotlinScript { api ->
-        api.space().projects.automation.deployments.start(
-            project = api.projectIdentifier(),
-            targetIdentifier = TargetIdentifier.Key("do-frontend-deployment"),
-            version = "1.0.0",
-            // automatically update deployment status based on a status of a job
-            syncWithAutomationJob = true
-        )
+import jetbrains.space.konfiguration.*
+import jetbrains.space.vcs.checkout.GitCheckout
+import jetbrains.space.vcs.checkout.GitHubCheckout
+
+spaceApplication {
+
+    // Define the deployment configuration for the Angular app
+    deployment("angular-deployment") {
+        spec {
+            container {
+                name = "angular-container"
+                image = "node:14-alpine"
+                command = "npm"
+                args = listOf("start")
+                ports = listOf(4200)
+                env = mapOf(
+                    "NODE_ENV" to "production"
+                )
+            }
+        }
+        workspace {
+            checkout(GitHubCheckout(
+                "jetbrains",
+                "do_frontend",
+                branch = "main",
+                readOnly = true
+            ))
+        }
+    }
+    
+    // Define the dev environment where the Angular app will be deployed
+    environment("dev") {
+        spec {
+            from {
+                image = "ubuntu:20.04"
+            }
+        }
+        workspace {
+            checkout(GitCheckout(
+                repository = "https://github.com/Plazmodium/do_frontend.git",
+                branch = "dev",
+                readOnly = true
+            ))
+        }
+    }
+    
+    // Define the staging environment where the Angular app will be deployed
+    environment("staging") {
+        spec {
+            from {
+                image = "ubuntu:20.04"
+            }
+        }
+        workspace {
+            checkout(GitCheckout(
+                repository = "https://github.com/Plazmodium/do_frontend.git",
+                branch = "staging",
+                readOnly = true
+            ))
+        }
+    }
+    
+    // Define the production environment where the Angular app will be deployed
+    environment("production") {
+        spec {
+            from {
+                image = "ubuntu:20.04"
+            }
+        }
+        workspace {
+            checkout(GitCheckout(
+                repository = "https://github.com/Plazmodium/do_frontend.git,
+                branch = "main",
+                readOnly = true
+            ))
+        }
     }
 }
